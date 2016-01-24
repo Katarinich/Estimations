ea2a731f05b10f7db12eb92f71f8ec931a435fbf
Katarinich <katarina.chirich@gmail.com>
2016-01-24 06:52:58 +0300

    Added adding new lines and sum calculating. Still almost working app

--- client/views/estimations/estimation_item.js
+++ client/views/estimations/estimation_item.js
@@ -1,3 +1,8 @@
+Template.estimationItem.rendered = function() {
+    Session.set('currentEstimation', this._id);
+    Meteor.typeahead.inject();
+};
+
 Template.estimationItem.helpers({
 	'lineIsEmpty' : function(){
         return this.text == "";
@@ -8,32 +13,64 @@ Template.estimationItem.helpers({
 				this.blocks
 			}
 		});
+	},
+	'sum': function(){
+		return Number(this.rate) * Number(this.hours); 
 	}
 });
 
 Template.estimationItem.events({
-	'click .record-text-div' : function(e){
+	'click .record-text-div, click .record-hours-div, click .record-rate-div' : function(e){
 		var valueToEdit = e.target.attributes[0].value.split("-")[1];
 		Session.set('valueToEdit', valueToEdit);
+		console.log(valueToEdit);
+
         e.target.innerHTML = "<input class='record-" + valueToEdit + " mousetrap' type='text' id=" + this._id + " value='" + this[valueToEdit] + "' />";
+
         document.getElementsByClassName("record-" + valueToEdit)[0].focus();
+        e.stopImmediatePropagation();
+        return false;
 	},
-	'blur .record-text' : function(e){
+	'blur .record-text, blur .record-hours, blur .record-rate' : function(e){
         var currentBlock = Blocks.findOne({_id: e.target.id});
         var newValue = e.target.value;
 
-        var valueToEdit = Session.get('valueToEdit');
-        currentBlock[valueToEdit] = newValue;
-        if(currentBlock.isParent){
-            document.getElementById(currentBlock._id).getElementsByClassName("record-text-div")[0].innerHTML = "<b>" + newValue + "<b>";
-        }
-        else {
-            document.getElementById(currentBlock._id).getElementsByClassName("record-text-div")[0].innerHTML = newValue;
-        }
+        //нужно придумать что-нибудь с этим =(
+        var valueToEdit = Session.get('valueToEdit').split(" ")[0];
 
-        /*if(newValue != currentBlock[valueToEdit]){
-            RecordsList.update({_id: currentBlock._id}, {$set: {text: newText}});
-            Lists.update({_id: currentBlock.listId}, {$set: {dateUpdated: new Date()}});
-        }*/
+        if(newValue != currentBlock[valueToEdit]){
+        	document.getElementById(currentBlock._id).getElementsByClassName("record-" + valueToEdit + "-div")[0].innerHTML = "";
+            Meteor.call('blockUpdate', currentBlock._id, valueToEdit, newValue, function(error) {
+        		if (error){
+        			throwError(error.reason);
+        		}
+       	 	});
+            //Lists.update({_id: currentBlock.listId}, {$set: {dateUpdated: new Date()}});
+        }
+        else document.getElementById(currentBlock._id).getElementsByClassName("record-" + valueToEdit + "-div")[0].innerHTML = newValue;
+        e.stopImmediatePropagation();
+	},
+	'keypress .record-rate' : function(e){
+		if (e.keyCode == 13) {
+			var currentBlock = Blocks.findOne({_id: e.target.id});
+			var currentEstimation = Estimations.findOne({_id: currentBlock.estimationId});
+			var parentBlock = Blocks.findOne({_id: currentBlock.parentId});
+
+			Meteor.call('blockInsert', currentEstimation._id, parentBlock._id, currentBlock.nesting, function(error, result) {
+        		if (error){
+        			throwError(error.reason);
+        		}
+
+        		var newBlockId = result;
+        		parentBlock.blocks.splice(parentBlock.blocks.indexOf(currentBlock._id), 0, newBlockId);
+
+       	 		Meteor.call('blockUpdate', parentBlock._id, "blocks", parentBlock.blocks, function(error) {
+        			if (error){
+        				throwError(error.reason);
+        			}
+       	 		});
+       	 	});
+		}
+		e.stopImmediatePropagation();
 	}
 });
--- client/views/estimations/estimation_page.js
+++ client/views/estimations/estimation_page.js
@@ -7,3 +7,33 @@ Template.estimationPage.helpers({
 		});
 	}
 });
+
+Template.estimationPage.events({
+	'click .list-name' : function(e){
+        var currentText = e.target.innerHTML;
+        e.target.innerHTML = "<input type='text' class='list-name-input' value='" + currentText +"' />";
+        document.getElementsByClassName("list-name-input")[0].focus();
+    },
+    'blur .list-name-input' : function(e){
+        var newText = e.target.value;
+        if(newText == this.name) {
+            document.getElementsByClassName("list-name")[0].innerHTML = newText;
+        }
+        else {
+            document.getElementsByClassName("list-name")[0].innerHTML="";
+            //Lists.update({_id: currentList._id}, {$set: {name: newText, dateUpdated: new Date()}});
+            Meteor.call('estimationUpdate', this._id, "name", newText, function(error) {
+        		if (error){
+        			throwError(error.reason);
+        		}
+       	 	});
+        }
+    },
+	'click .client-name-label-div' : function(e){
+        var currentText = e.target.innerHTML;
+        document.getElementsByClassName("client-name-label-div")[0].style.display = "none";
+        document.getElementsByClassName("client-name-input")[1].style.display = 'inline-block';
+        document.getElementsByClassName("client-name-input")[1] = currentText;
+        document.getElementsByClassName("client-name-input")[1].focus();
+    }
+});
